515a11f79c71483dde9e33f3ec1c929de4d42505
wevanbrown <wevanbrown@gmail.com>
2021-09-15 17:10:05 -0400

    Added another test to bottest, made ping.py testable

--- cogs/ping.py
+++ cogs/ping.py
@@ -8,7 +8,7 @@ class Helpful(commands.Cog):
 
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send(f"Pong! My ping currently is {round(self.bot.latency * 1000)}ms")
+        await ctx.send(f"Pong! My ping currently is {round(min(999999999 , self.bot.latency * 1000))}ms")
 
 def setup(bot):
     bot.add_cog(Helpful(bot))
--- test/bottest.py
+++ test/bottest.py
@@ -9,3 +9,10 @@ import pytest
 async def test_hello(bot):
     await dpytest.message("$hello")
     assert dpytest.verify().message().content("Hello World!")
+
+# Tests cogs/ping.py
+@pytest.mark.asyncio
+async def test_ping(bot):
+    await dpytest.message("$ping")
+    assert dpytest.verify().message().contains().content("Pong!")
+
