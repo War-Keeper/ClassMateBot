787ab637c58c0bb196c160eabf4eeba122075004
sunil1511 <sunilupare45@gmail.com>
2021-09-29 22:42:48 -0400

    Comment in bot.py

--- bot.py
+++ bot.py
@@ -72,7 +72,7 @@ async def on_member_join(member):
     unverified = discord.utils.get(
         member.guild.roles, name=UNVERIFIED_ROLE_NAME
     )  # finds the unverified role in the guild
-    await member.add_roles(unverified)
+    await member.add_roles(unverified) # assigns the unverified role to the new member 
     await member.send("Hello " + member.name + "!")
     await member.send(
         "Verify yourself before getting started! \n To use the verify command, do: $verify <your_full_name> \n \
