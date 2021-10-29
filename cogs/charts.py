6a84e456c2e99bf7c251fee0074d2942502bc10d
Niraj Lavani <60410421+nirajlavani@users.noreply.github.com>
2021-10-29 17:23:33 -0400

    Chart.py grades admin tool improvement: add grades in $grades command. Restricted command to admin only

--- cogs/charts.py
+++ cogs/charts.py
@@ -11,6 +11,7 @@ class Charts(commands.Cog):
     @commands.command(name="grades",
                       help="View grade distribution; FORMAT (7 inputs): chart_type (pie, bar, line), title (1 word),"
                            "number of As, number of Bs, number of Cs, number of Ds, number of Fs")
+    @commands.has_permissions(administrator=True)
     async def grades(self, ctx, chart: str, aGrade: int, bGrade: int, cGrade: int, dGrade: int, fGrade: int):
         qc = QuickChart()
         qc.width = 500
@@ -21,15 +22,21 @@ class Charts(commands.Cog):
             "data": {
                 "labels": ["A", "B", "C", "D", "F"],
                 "datasets": [{
+                    "backgroundColor": ['rgb(128, 177, 229)',
+                                        'rgb(116, 232, 219)',
+                                        'rgb(246, 220, 154)',
+                                        'rgb(250, 195, 149)',
+                                        'rgb(245, 165, 145)'],
                     "label": "grades",
                     "data": [aGrade, bGrade, cGrade, dGrade, fGrade]
                 }]
             }
-        }
 
+        }
         link = qc.get_url()
         shortener = pyshorteners.Shortener()
         shortened_link = shortener.tinyurl.short(link)
+
         await ctx.send(f"{shortened_link}")
 
     @grades.error
@@ -39,6 +46,7 @@ class Charts(commands.Cog):
                 "FORMAT: $grades chart_type (pie, bar, line), title (1 word), "
                 "number of As, number of Bs, number of Cs, number of Ds, number of Fs \n"
                 "\n EXAMPLE: $grades bar 5 4 3 2 1")
+
 # -------------------------------------
 # add the file to the bot's cog system
 # -------------------------------------
