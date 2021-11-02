155830adac8976d561ab58a1e8ef9feaf3d76a4e
05sandesh <mailtosandeshas@gmail.com>
2021-11-02 18:55:26 -0400

    editing regex

--- cogs/links.py
+++ cogs/links.py
@@ -8,8 +8,8 @@ import re
 
 
 # -----------------------------------------------------------
-# This File contains commands for voting on projects,
-# displaying which groups have signed up for which project
+# This File contains commands for Saving the links in a file,
+# Display all the messages which contains links.
 # -----------------------------------------------------------
 class links(commands.Cog):
     def __init__(self, bot):
@@ -20,13 +20,6 @@ class links(commands.Cog):
         url=[]
         message_links = []
         temp=[]
-        '''regex = re.compile(
-            r'^(?:http|ftp)s?://'  # http:// or https://
-            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-            r'localhost|'  # localhost...
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-            r'(?::\d+)?'  # optional port
-            r'(?:/?|[/?]\S+)$', re.IGNORECASE)'''
         regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
         url = re.findall(regex, message.content)
         test= re.findall(regex, message.content) is not None
